49805bc987a69d0edbc87b1f46b78f2dca045f95
Ayrene <112762086+ayreneYS@users.noreply.github.com>
2023-06-07 22:57:37 +0800

    PHP & MySQL

--- article.php
+++ article.php
@@ -1,40 +1,44 @@
-<!-- Query the Database from PHP And Get the Results -->
 <?php
 
 require 'includes/database.php';
 
+$conn = getDB();
+
+
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 
-$sql = "SELECT *
-        FROM article
-        WHERE id = " . $_GET['id'];
-     
+    $sql = "SELECT *
+            FROM article
+            WHERE id = " . $_GET['id'];
 
-$results = mysqli_query($conn, $sql);
+    $results = mysqli_query($conn, $sql);
 
-if ($results === false) {
-    echo mysqli_error($conn);
+    if ($results === false) {
+
+        echo mysqli_error($conn);
+
+    } else {
+
+        $article = mysqli_fetch_assoc($results);
+
+    }
+
 } else {
-    $article = mysqli_fetch_assoc($results);
-}
-
-} else {
-    $article = null; 
+    $article = null;
 }
 
 ?>
-
 <?php require 'includes/header.php'; ?>
 
-        <?php if ($article === null): ?>
-            <p>No articles found.</p>
-        <?php else: ?>
+<?php if ($article === null): ?>
+    <p>Article not found.</p>
+<?php else: ?>
 
-            <article>
-                <h2><?= $article['title']; ?></h2>
-                <p><?= $article['content']; ?></p>
-            </article>
+    <article>
+        <h2><?= $article['title']; ?></h2>
+        <p><?= $article['content']; ?></p>
+    </article>
 
-        <?php endif; ?>
+<?php endif; ?>
 
 <?php require 'includes/footer.php'; ?>
